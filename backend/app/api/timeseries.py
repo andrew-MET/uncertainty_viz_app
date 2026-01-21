c506d1380a5990c5d2108077d04b35eac4c89e15
Andrew Singleton <andrewts@met.no>
2026-01-21 10:38:10 +0100

    Remove model param from deode endpoint

--- backend/app/api/timeseries.py
+++ backend/app/api/timeseries.py
@@ -117,7 +117,6 @@ async def get_timeseries(
   lat: float = Query(..., ge = -90, le = 90),
   lon: float = Query(..., ge = -180, le = 180),
   dttm: str = Query(..., pattern = r"\d{10}$"),
-  model: str = Query(...),
   param: Literal["temp", "press", "cloud", "precip", "wind"] = Query(...)
 ):
   
